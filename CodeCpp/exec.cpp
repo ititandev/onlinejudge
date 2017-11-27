69fe7fda5f8c6a01e1de40669536451da71d8ee9
tadevi <1611695@hcmut.edu.vn>
2017-11-27 09:05:15 +0700

    fix some errors

--- CodeCpp/exec.cpp
+++ CodeCpp/exec.cpp
@@ -9,26 +9,16 @@
 #include <array>
 using namespace std;
 
-string Run(const string& cmd, bool& success, const string& logfile)
+void Run(const string& cmd, bool& success, const string& logfile)
 {
     try
   	{
     		string com=cmd+" &>> "+logfile;
     		int succ=system(com.c_str());
-    		fstream f(logfile,ios::out|ios::app);
-    		string output;
-    		string tmp;
-    		while(!f.eof())
-    		{
-        		getline(f,tmp);
-      	 		output+=tmp+"\n";
-    		} 
     		success=(succ==0);
-    		return output;
-    	}
+    }
     catch(exception& e)
     {
-
         throw;
     }
 }
